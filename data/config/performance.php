7590a337b9f358ee4e00f3f6afb3f0d1a98c4f31
xj <908634674@qq.com>
2017-09-19 07:13:27 +0800

    修改默认配置

--- data/config/performance.php
+++ data/config/performance.php
@@ -3,7 +3,7 @@
 return array (
 	'DEBUG'=>true,	//是否开启调试模式
 	'LOG_ON'=>true,	//是否开启出错信息保存到文件
-	'REWRITE_ON'=>true,	//伪静态开关
+	'REWRITE_ON'=>false,	//伪静态开关
 	'COOKIE_PREFIX'=>'6puxa_', // COOKIE前缀
     'SAFE_KEY'=>'kniALCwvhPu3r08WRix3', // 加密码
 	'API_SWITCH'=>false,
